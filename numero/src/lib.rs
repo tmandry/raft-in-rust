d17a0db79e08bc55e3e4e671c89107ab2f56408b
Tyler Mandry <tmandry@gmail.com>
2018-10-28 16:48:06 -0500

    Sketch out basic StateMachine

--- numero/src/lib.rs
+++ numero/src/lib.rs
@@ -1,7 +1,52 @@
+#![allow(dead_code)]
+
+use raft::{StateMachine};
+
+struct NumberService {
+    last: Option<u64>,
+}
+
+impl NumberService {
+    fn new() -> NumberService {
+        NumberService {
+            last: None,
+        }
+    }
+
+    fn next_number(&mut self) -> u64 {
+        let next = match self.last {
+            Some(last) => last + 1,
+            None       => 1,
+        };
+        self.last = Some(next);
+        next
+    }
+}
+
+enum Command {
+    GiveMeANumber,
+}
+
+impl StateMachine for NumberService {
+    type Command = Command;
+    type Response = u64;
+
+    fn apply(&mut self, command: &Self::Command) -> Self::Response {
+        match command {
+            Command::GiveMeANumber => self.next_number()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut svc = NumberService::new();
+        assert_eq!(1, svc.next_number());
+        assert_eq!(2, svc.next_number());
+        assert_eq!(3, svc.next_number());
     }
 }
--- raft/src/lib.rs
+++ raft/src/lib.rs
@@ -1,3 +1,21 @@
+#![allow(dead_code)]
+
+pub trait StateMachine {
+    type Command;
+    type Response;
+
+    fn apply(&mut self, command: &Self::Command) -> Self::Response;
+}
+
+struct Server<S: StateMachine> {
+    log: Vec<S::Command>,
+}
+
+trait Apply<T> {
+    type Result;
+    fn apply(&mut self, command: &T) -> Self::Result;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
