ded8c6bdbee7756630b2032874072417a0235e4e
Tyler Mandry <tmandry@gmail.com>
2018-11-08 12:43:56 -0600

    Add lower_bound to GiveMeANumber request

--- numero/src/lib.rs
+++ numero/src/lib.rs
@@ -1,5 +1,6 @@
 use raft::StateMachine;
 use serde_derive::{Deserialize, Serialize};
+use std::cmp::{max, min};
 
 struct NumberService {
     last: Option<u64>,
@@ -7,17 +8,21 @@ struct NumberService {
 
 #[allow(dead_code)]
 impl NumberService {
+    const MAX_LOWER_BOUND: u64 = 1_000_000;
+
     fn new() -> NumberService {
         NumberService { last: None }
     }
 
-    fn next_number(&mut self) -> u64 {
-        let next = match self.last {
-            Some(last) => last + 1,
-            None => 1,
-        };
+    fn next_number(&mut self, lower_bound: Option<u64>) -> u64 {
+        let lower_bound = min(lower_bound.unwrap_or(0), Self::MAX_LOWER_BOUND);
+        let next = max(lower_bound, self.last.unwrap_or(0) + 1);
         self.last = Some(next);
         next
+    }
+
+    fn last_issued(&self) -> Option<u64> {
+        self.last
     }
 }
 
@@ -29,7 +34,7 @@ impl Default for NumberService {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Command {
-    GiveMeANumber,
+    GiveMeANumber(Option<u64>),
     LastNumberIssued,
 }
 
@@ -39,8 +44,8 @@ impl StateMachine for NumberService {
 
     fn apply(&mut self, command: &Self::Command) -> Self::Response {
         match command {
-            Command::GiveMeANumber => Some(self.next_number()),
-            Command::LastNumberIssued => self.last,
+            Command::GiveMeANumber(lower_bound) => Some(self.next_number(*lower_bound)),
+            Command::LastNumberIssued => self.last_issued(),
         }
     }
 }
@@ -52,19 +57,21 @@ mod tests {
     #[test]
     fn next_number() {
         let mut svc = NumberService::new();
-        assert_eq!(1, svc.next_number());
-        assert_eq!(2, svc.next_number());
-        assert_eq!(3, svc.next_number());
+        assert_eq!(1, svc.next_number(None));
+        assert_eq!(2, svc.next_number(None));
+        assert_eq!(3, svc.next_number(None));
     }
 
     #[test]
     fn apply() {
         let mut svc = NumberService::new();
         assert_eq!(None, svc.apply(&Command::LastNumberIssued));
-        assert_eq!(Some(1), svc.apply(&Command::GiveMeANumber));
-        assert_eq!(Some(2), svc.apply(&Command::GiveMeANumber));
-        assert_eq!(Some(3), svc.apply(&Command::GiveMeANumber));
+        assert_eq!(Some(1), svc.apply(&Command::GiveMeANumber(None)));
+        assert_eq!(Some(2), svc.apply(&Command::GiveMeANumber(None)));
+        assert_eq!(Some(3), svc.apply(&Command::GiveMeANumber(None)));
         assert_eq!(Some(3), svc.apply(&Command::LastNumberIssued));
-        assert_eq!(Some(4), svc.apply(&Command::GiveMeANumber));
+        assert_eq!(Some(4), svc.apply(&Command::GiveMeANumber(None)));
+        assert_eq!(Some(100), svc.apply(&Command::GiveMeANumber(Some(100))));
+        assert_eq!(Some(100), svc.apply(&Command::LastNumberIssued));
     }
 }
